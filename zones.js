5becc016d06c8f149ca8eb2fa14c704f41c077ce
Juozas Kripas <2005.joe@gmail.com>
2016-04-07 16:04:37 -0600

    JS Making a new function showTime
    assigned a current date to var newTIme, then grabbed the Date object
    from var newTime and added it to the milliSeconds parameter

---
+++ zones.js
@@ -0,0 +1,27 @@
+/*
+   New Perspectives on HTML, XHTML, and DHTML
+   Tutorial 11
+   Case Problem 3
+
+   Author: Juozas Kripas
+   Date:   4/7/2016
+
+   Function List:
+   addTime(oldtime, milliseconds)
+      Used to add a specified number of milliseconds to a date object named oldtime.
+      A new date object with the new time value is returned by the function.
+
+   showTime(time)
+      Displays a time value in the format:
+      hh:mm AM/PM
+*/
+
+function addTime(oldTime, milliSeconds){
+    var newTime = new Date();
+    var newValue= oldTime.getTime + milliSeconds;
+}
+
+
+
+
+
